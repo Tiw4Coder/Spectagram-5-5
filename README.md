f0cb217da31778388563e2ca489549cfe0a14034
Tiw4Coder <70450941+Tiw4Coder@users.noreply.github.com>
2021-12-11 17:17:50 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Spectagram-5-5
+# spectagram-stage-1
+project solution for c81
---
+++ config.js
@@ -0,0 +1,12 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyB1dDI8bIZ0g7XP6oUwf_lBVa1bfwjfZXo",
+    authDomain: "spectagram-b772f.firebaseapp.com",
+    databaseURL: "https://spectagram-b772f-default-rtdb.firebaseio.com",
+    projectId: "spectagram-b772f",
+    storageBucket: "spectagram-b772f.appspot.com",
+    messagingSenderId: "680277321084",
+    appId: "1:680277321084:web:60ee947455d93e3fbb30f9"
+  };
+  
+  // Initialize Firebase
+  
